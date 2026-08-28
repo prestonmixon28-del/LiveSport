0d949779c6d1c763c0b8809f91bd129c5770382c
prestonmixon28-del <prestonmixon28@gmail.com>
2026-08-28 13:46:05 -0500

    phase-3 finish

--- Journal.md
+++ Journal.md
@@ -3,3 +3,5 @@ Phase-1:
 Keeping them separate makes the code easier to understand and changed. The gameticker can focus on storing updates while the notifications are separate. This would make it easier to reuse and maintain.
 Phase-2:
 The interface create a common set of rules. The subject can notify any Observer without needing to know which specific class it is. It would make the program easier to change and add new observers.
+Phase-3:
+The GameTicker only needs to know that each object is an Observer. It can notify all of them, without knowing what type they are. This makes the program easier to read.
--- src/GameTicker.java
+++ src/GameTicker.java
@@ -1,17 +1,34 @@
 import java.util.ArrayList;
 
-public class GameTicker {
+public class GameTicker implements Subject {
 
     private ArrayList<String> updates = new ArrayList<>();
+    private ArrayList<Observer> observers = new ArrayList<>();
+
+    public void register(Observer o) {
+        observers.add(o);
+        }
+
+    public void remove(Observer o) {
+        observers.remove(o);
+    }
+
+    public void notifyObservers() {
+        for (Observer observer : observers) {
+            observer.update();
+        }
+    }
 
     public void addUpdate(String text) {
         updates.add(text);
+        notifyObservers();
     }
 
     public String getLatestUpdate() {
         if (updates.isEmpty()) {
             return "no updates yet";
         }
+
         return updates.get(updates.size() - 1);
     }
 }
